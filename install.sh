89f4586b59711f8cb32ce7dca64f09fd4908ab39
Viktor Jensen-Torp <v.jensentorp@gmail.com>
2023-12-28 15:26:08 +0100

    update files

--- install.sh
+++ install.sh
@@ -1,73 +1,26 @@
 #!/bin/bash
 
-echo "What are you bootstrapping? (Type number)"
-options=("Personal machine" "Server")
+sudo apt update
 
-select opt in "${options[@]}"
-do
-    case $opt in
-        "Client")
-            echo "Installing everything with apt..."
-            # Add installation commands here
-            sudo apt update
+# Check if Zsh is installed
+if [ -f /bin/zsh ] || [ -f /usr/bin/zsh ]; then
+    echo "Zsh already installed"
+else
+    echo "Installing Zsh"
+    sudo apt install zsh -y
+    exit 1
+fi
 
-            # Check if Zsh is installed
-            if [ -f /bin/zsh ] || [ -f /usr/bin/zsh ]; then
-                echo "Zsh already installed"
-            else
-                echo "Installing Zsh"
-                sudo apt install zsh -y
-                exit 1
-            fi
-            
-            # Install git
-            sudo apt install git -y
+# Install pyenv
 
-            # Install pyenv
+# MacOS
+# brew install pyenv
 
-            # MacOS
-            # brew install pyenv
+# Ubuntu 
+# curl https://pyenv.run | bash
 
-            # Ubuntu 
-            # curl https://pyenv.run | bash
-
-            # Install latest python 3 version
-            # pyenv install 3:latest
-            
-            break
-            ;;
-        "Server")
-            echo "Installing server stuff with yum..."
-            # Add installation commands here
-            sudo yum update -y
-
-            # Check if Zsh is installed
-            if [ -f /bin/zsh ] || [ -f /usr/bin/zsh ]; then
-                echo "Zsh already installed"
-            else
-                echo "Installing Zsh"
-                sudo yum install zsh -y
-                exit 1
-            fi
-
-            # Install git
-            sudo yum install git -y
-
-            # if chsh (change shell) isn't available: install util-linux-user which has it
-            if ! command -v chsh &> /dev/null
-            then
-                sudo yum install util-linux-user -y
-            fi
-
-            break
-            ;;
-        *) echo "Invalid option $REPLY";;
-    esac
-    break
-done
-
-git config --global user.name "Viktor Jensen-Torp"
-git config --global user.email "v.jensentorp@gmail.com"
+# Install latest python 3 version
+# pyenv install 3:latest
 
 # Set zsh as default shell
 sudo chsh -s $(which zsh) $(whoami)
@@ -77,7 +30,6 @@ sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/maste
 
 # Install starship prompt
 sudo sh -c "$(curl -fsSL https://starship.rs/install.sh)"
-echo '\neval "$(starship init bash)"' >> .bashrc
 
 # Install nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
