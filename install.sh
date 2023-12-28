4d2cfdf0bcfc262e5036d7869f7cc2e63a3dff4a
Viktor Jensen-Torp <v.jensentorp@gmail.com>
2023-12-28 12:50:49 +0100

    fix: server configuration to install script

--- install.sh
+++ install.sh
@@ -1,37 +1,59 @@
 #!/bin/bash
 
+echo "What are you bootstrapping?"
+options=("Personal machine" "Server")
+
+select opt in "${options[@]}"
+do
+    case $opt in
+        "Client")
+            echo "Installing everything with apt..."
+            # Add installation commands here
+            sudo apt update
+            sudo apt install zsh -y
+
+            # Install pyenv
+
+            # MacOS
+            brew install pyenv
+
+            # Ubuntu 
+            curl https://pyenv.run | bash
+
+            # Install latest python 3 version
+            pyenv install 3:latest
+            
+            break
+            ;;
+        "Server")
+            echo "Installing server stuff with yum..."
+            # Add installation commands here
+            sudo yum update
+            sudo yum install zsh -y
+
+            break
+            ;;
+        *) echo "Invalid option $REPLY";;
+    esac
+    break
+done
+
 # Install zsh
-sudo apt update
-sudo apt install zsh -y
 
 # Set zsh as default shell
 chsh -s $(which zsh)
 
 # Install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # Install starship prompt
-sh -c "$(curl -fsSL https://starship.rs/install.sh)"
-
-
-# Install pyenv
-
-# MacOS
-brew install pyenv
-
-# Ubuntu 
-# curl https://pyenv.run | bash
-
-# Install latest python 3 version
-pyenv install 3:latest
-
+sudo sh -c "$(curl -fsSL https://starship.rs/install.sh)"
 
 # Install nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
 
 # Install  latest node
 nvm install node
-
 
 # Install neovim
 sudo apt install neovim -y
