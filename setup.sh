136a1cdbe24cadf10de069f4aa0627abe6d28276
ViktorJT <v.jensentorp@gmail.com>
2025-03-03 19:51:43 +0100

    testing

--- setup.sh
+++ setup.sh
@@ -1,40 +1,40 @@
 #!/bin/bash
 set -e
 
-REPO_URL="https://github.com/viktorjt/dotfiles.git"
+echo "$HOME"
 
 # Detect if /tmp is noexec and create an alternative tmp directory if needed
-if mount | grep '/tmp' | grep -q 'noexec'; then
-  mkdir -p "$HOME/.local/share/chezmoi/tmp"
-fi
+# if mount | grep '/tmp' | grep -q 'noexec'; then
+#  mkdir -p "$HOME/.local/share/chezmoi/tmp"
+# fi
 
 # Parse arguments
-ENVIRONMENT="macos"
-SSH_KEY_NAME=""
+# ENVIRONMENT="macos"
+# SSH_KEY_NAME=""
 
-for arg in "$@"; do
-  case $arg in
-    --env=*)
-      ENVIRONMENT="${arg#*=}"
-      shift
-      ;;
-    --ssh-key=*)
-      SSH_KEY_NAME="${arg#*=}"
-      shift
-      ;;
-  esac
-done
+# for arg in "$@"; do
+#  case $arg in
+#    --env=*)
+#      ENVIRONMENT="${arg#*=}"
+#      shift
+#      ;;
+#    --ssh-key=*)
+#      SSH_KEY_NAME="${arg#*=}"
+#      shift
+#      ;;
+#  esac
+#done
 
 
-echo "Setting up environment: $ENVIRONMENT"
+#echo "Setting up environment: $ENVIRONMENT"
 
 # Install chezmoi and apply dotfiles in one step
-sh -c "$(curl -fsLS get.chezmoi.io)" -- -b $HOME/.local/bin
+# sh -c "$(curl -fsLS get.chezmoi.io)" -- -b $HOME/.local/bin
 
 # Initialize chezmoi (defaults to dotfiles repo when passing a Github username)
-chezmoi init ViktorJT
+# chezmoi init ViktorJT
 
 # Apply dotfiles with the environment variable
-chezmoi apply -- --env=$ENVIRONMENT
+# chezmoi apply -- --env=$ENVIRONMENT
 
-echo "Dotfiles setup complete!"
+# echo "Dotfiles setup complete!"
