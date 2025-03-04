2a1b801b53e72e95d32a34cbcd95a5b1b15d18fa
ViktorJT <v.jensentorp@gmail.com>
2025-03-04 22:21:58 +0100

    test

--- setup.sh
+++ setup.sh
@@ -12,34 +12,10 @@ for arg in "$@"; do
 done
 
 if ! command -v chezmoi &> /dev/null; then
-  sh -c "$(curl -fsLS get.chezmoi.io)" -- -b "$HOME/.local/bin"
+  sudo sh -c "$(curl -fsLS get.chezmoi.io)" -- -b "$HOME/.local/bin"
 fi
 
 export PATH="$HOME/.local/bin:$PATH"
 
-#ENV_DATA=$(curl -fsSL "https://raw.githubusercontent.com/ViktorJT/dotfiles/main/.chezmoidata/environments.yaml")
-
-#CONFIG_MODE=$(echo "$ENV_DATA" | awk -v env="$ENVIRONMENT" '
-#  $1 == "environments:" { in_env=1 } 
-#  in_env && $1 == env ":" { found=1 }
-#  found && $1 == "config:" { print $2; exit }
-#')
-
-#if [[ "$CONFIG_MODE" == "local" ]]; then
-#  chezmoi init --source="$EXECUTION_DIR" ViktorJT
-#else
-#  chezmoi init ViktorJT
-#fi
-
 chezmoi init ViktorJT
-chezmoi apply
-
-# Apply chezmoi configuration
-#if [[ "$CONFIG_MODE" == "local" ]]; then
-#  LOCAL_CONFIG_PATH="$HOME/.config/chezmoi/chezmoi.toml"
-#  mkdir -p "$(dirname "$LOCAL_CONFIG_PATH")"
-#  echo "sourceDir = \"$EXECUTION_DIR\"" > "$LOCAL_CONFIG_PATH"
-#  CHEZMOI_ENV="$ENVIRONMENT" chezmoi apply --config="$LOCAL_CONFIG_PATH" --source="$EXECUTION_DIR"
-#else
-#  chezmoi apply
-#fi
+CHEZMOI_ENVIRONMENT="$ENVIRONMENT" chezmoi apply
